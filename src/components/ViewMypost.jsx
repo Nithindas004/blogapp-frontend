cbfad86e5b737dee0759c972a014d369323baf16
Nithindas004 <nithindas232001@gmail.com>
2024-02-10 15:07:37 +0530

    not completed

---
+++ src/components/ViewMypost.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ViewMypost = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewMypost
